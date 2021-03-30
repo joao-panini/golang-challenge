57ca49200fc84c2a494834c24cddadad4340b762
João Panini <joaogabrielpanini@gmail.com>
2021-03-30 17:10:54 -0300

    latest adjustments

--- src/model/transfers.go
+++ src/model/transfers.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"errors"
 	"time"
 )
 
@@ -11,15 +10,4 @@ type Transfers struct {
 	ToAccountID   uint64    `json:"ToAccountID,omitempty"`
 	Amount        float64   `json:"Amount,omitempty"`
 	Created_at    time.Time `json:"Created_at,omitempty"`
-}
-
-func (transfer *Transfers) Validate() error {
-
-	if transfer.FromAccountID <= 0 {
-		return errors.New("destinatario é obrigatorio")
-	}
-	if transfer.Amount <= 0 {
-		return errors.New("insira um valor de transferencia maior do que 0")
-	}
-	return nil
 }
